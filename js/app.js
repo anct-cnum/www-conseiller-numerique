338ead6b7e502ea31833e4cf5832e80c150964a7
Sébastien Quirrion <sebastien@fitlab.fr>
2021-02-08 11:43:18 +0100

    modif header et footer

--- js/app.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-    $("div[data-includeHTML]").each(function () {    
-        $(this).load($(this).attr("data-includeHTML"));
-        $(this).removeAttr("data-includeHTML");
-    });
-});
